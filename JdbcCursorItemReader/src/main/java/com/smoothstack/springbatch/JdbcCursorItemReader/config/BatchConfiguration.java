3f40c767865bcf629cee6dc171a2ff4027fe5752
sessums-smoothstack <99749819+sessums-smoothstack@users.noreply.github.com>
2022-03-12 15:23:33 -0500

    Spring Batch JDBC cursor item reader demo

---
+++ JdbcCursorItemReader/src/main/java/com/smoothstack/springbatch/JdbcCursorItemReader/config/BatchConfiguration.java
@@ -0,0 +1,71 @@
+package com.smoothstack.springbatch.JdbcCursorItemReader.config;
+
+import com.smoothstack.springbatch.JdbcCursorItemReader.listener.HwJobExecutionListener;
+import com.smoothstack.springbatch.JdbcCursorItemReader.listener.HwStepExecutionListener;
+import com.smoothstack.springbatch.JdbcCursorItemReader.model.Transaction;
+import com.smoothstack.springbatch.JdbcCursorItemReader.writer.ConsoleItemWriter;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+
+import javax.sql.DataSource;
+
+@EnableBatchProcessing
+@org.springframework.context.annotation.Configuration
+public class BatchConfiguration {
+
+    @Autowired
+    private JobBuilderFactory jobs;
+
+    @Autowired
+    private StepBuilderFactory steps;
+
+    @Autowired
+    private HwJobExecutionListener hwJobExecutionListener;
+
+    @Autowired
+    private HwStepExecutionListener hwStepExecutionListener;
+
+    @Autowired
+    private DataSource dataSource;
+
+
+    @Bean
+    public JdbcCursorItemReader jdbcCursorItemReader(){
+        JdbcCursorItemReader reader = new JdbcCursorItemReader();
+        reader.setDataSource(this.dataSource);
+        reader.setSql("select * from transaction_pool.transactions");
+        reader.setRowMapper(new BeanPropertyRowMapper(){
+            {
+                setMappedClass(Transaction.class);
+            }
+        });
+        return reader;
+    }
+
+    @Bean
+    public Step jdbcCursorStep(){
+        return steps.get("jdbcCursorStep")
+                .listener(hwStepExecutionListener)
+                .<Integer,Integer>chunk(1000)
+                 .reader(jdbcCursorItemReader())
+                .writer(new ConsoleItemWriter())
+                .build();
+    }
+
+    @Bean
+    public Job jdbcCursorJob(){
+        return jobs.get("jdbcCursorJob")
+                .incrementer(new RunIdIncrementer())
+                .listener(hwJobExecutionListener)
+                .start(jdbcCursorStep())
+                .build();
+    }
+}
---
+++ JdbcCursorItemReader/src/main/java/com/smoothstack/springbatch/JdbcCursorItemReader/model/Transaction.java
@@ -0,0 +1,248 @@
+package com.smoothstack.springbatch.JdbcCursorItemReader.model;
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @Column
+    private String dte;
+
+    @Column
+    private String country;
+
+    @Column
+    private String area;
+
+    @Column
+    private String number;
+
+    @Column
+    private String ttl_area;
+
+    @Column
+    private String avg_area;
+
+    @Column
+    private String ttl_trans_amt;
+
+    @Column
+    private String min_trans_amt;
+
+    @Column
+    private String max_trans_amt;
+
+    @Column
+    private String unit_price_min;
+
+    @Column
+    private String unit_price_max;
+
+    @Column
+    private String unit_price_med;
+
+    @Column
+    private String unit_price_avg;
+
+    @Column
+    private String unit_price_std;
+
+    @Column
+    private String mnth;
+
+    @Column
+    private String yar;
+
+    @Column
+    private String index;
+
+    public Transaction() {
+    }
+
+    public Transaction(String dte, String country, String area, String number, String ttl_area, String avg_area,
+                       String ttl_trans_amt, String min_trans_amt, String max_trans_amt, String unit_price_min,
+                       String unit_price_max, String unit_price_med, String unit_price_avg, String unit_price_std,
+                       String mnth, String yar, String index) {
+        this.dte = dte;
+        this.country = country;
+        this.area = area;
+        this.number = number;
+        this.ttl_area = ttl_area;
+        this.avg_area = avg_area;
+        this.ttl_trans_amt = ttl_trans_amt;
+        this.min_trans_amt = min_trans_amt;
+        this.max_trans_amt = max_trans_amt;
+        this.unit_price_min = unit_price_min;
+        this.unit_price_max = unit_price_max;
+        this.unit_price_med = unit_price_med;
+        this.unit_price_avg = unit_price_avg;
+        this.unit_price_std = unit_price_std;
+        this.mnth = mnth;
+        this.yar = yar;
+        this.index = index;
+    }
+
+    public String getDate() {
+        return dte;
+    }
+
+    public void setDate(String dte) {
+        this.dte = dte;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getTtl_area() {
+        return ttl_area;
+    }
+
+    public void setTtl_area(String ttl_area) {
+        this.ttl_area = ttl_area;
+    }
+
+    public String getAvg_area() {
+        return avg_area;
+    }
+
+    public void setAvg_area(String avg_area) {
+        this.avg_area = avg_area;
+    }
+
+    public String getTtl_trans_amt() {
+        return ttl_trans_amt;
+    }
+
+    public void setTtl_trans_amt(String ttl_trans_amt) {
+        this.ttl_trans_amt = ttl_trans_amt;
+    }
+
+    public String getMin_trans_amt() {
+        return min_trans_amt;
+    }
+
+    public void setMin_trans_amt(String min_trans_amt) {
+        this.min_trans_amt = min_trans_amt;
+    }
+
+    public String getMax_trans_amt() {
+        return max_trans_amt;
+    }
+
+    public void setMax_trans_amt(String max_trans_amt) {
+        this.max_trans_amt = max_trans_amt;
+    }
+
+    public String getUnit_price_min() {
+        return unit_price_min;
+    }
+
+    public void setUnit_price_min(String unit_price_min) {
+        this.unit_price_min = unit_price_min;
+    }
+
+    public String getUnit_price_max() {
+        return unit_price_max;
+    }
+
+    public void setUnit_price_max(String unit_price_max) {
+        this.unit_price_max = unit_price_max;
+    }
+
+    public String getUnit_price_med() {
+        return unit_price_med;
+    }
+
+    public void setUnit_price_med(String unit_price_med) {
+        this.unit_price_med = unit_price_med;
+    }
+
+    public String getUnit_price_avg() {
+        return unit_price_avg;
+    }
+
+    public void setUnit_price_avg(String unit_price_avg) {
+        this.unit_price_avg = unit_price_avg;
+    }
+
+    public String getUnit_price_std() {
+        return unit_price_std;
+    }
+
+    public void setUnit_price_std(String unit_price_std) {
+        this.unit_price_std = unit_price_std;
+    }
+
+    public String getMonth() {
+        return mnth;
+    }
+
+    public void setMonth(String mnth) {
+        this.mnth = mnth;
+    }
+
+    public String getYear() {
+        return yar;
+    }
+
+    public void setYear(String yar) {
+        this.yar = yar;
+    }
+
+    public String getIndex() {
+        return index;
+    }
+
+    public void setIndex(String index) {
+        this.index = index;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "date='" + dte + '\'' +
+                ", country='" + country + '\'' +
+                ", area='" + area + '\'' +
+                ", number='" + number + '\'' +
+                ", ttl_area='" + ttl_area + '\'' +
+                ", avg_area='" + avg_area + '\'' +
+                ", ttl_trans_amt='" + ttl_trans_amt + '\'' +
+                ", min_trans_amt='" + min_trans_amt + '\'' +
+                ", max_trans_amt='" + max_trans_amt + '\'' +
+                ", unit_price_min='" + unit_price_min + '\'' +
+                ", unit_price_max='" + unit_price_max + '\'' +
+                ", unit_price_med='" + unit_price_med + '\'' +
+                ", unit_price_avg='" + unit_price_avg + '\'' +
+                ", unit_price_std='" + unit_price_std + '\'' +
+                ", month='" + mnth + '\'' +
+                ", year='" + yar + '\'' +
+                ", index='" + index + '\'' +
+                '}';
+    }
+}
